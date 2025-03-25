7a4502fbef0fe0da88dd50d20fddf08f267e020a
João Victor Alves <joaovictoralves552@gmail.com>
2025-03-25 11:30:59 -0300

    FIX gif loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as S from './Loading.Styled';
+import loadingGif from '../../assets/book.gif';
 
 export const Loading: React.FC = () => {
   return (
     <S.LoadingOverlay>
-      <S.Img src='src/assets/book.gif'></S.Img>
+      <S.Img src={loadingGif}></S.Img>
       <S.H1>Gestão Literária</S.H1>
     </S.LoadingOverlay>
   );
